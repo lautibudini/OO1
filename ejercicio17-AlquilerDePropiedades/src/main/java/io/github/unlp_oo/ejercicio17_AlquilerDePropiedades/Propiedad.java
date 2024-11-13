766f1cf1658bc5afcb0fea7953e95c809afa4eb6
Lautaro Budini <lautarobudini5@gmail.com>
2024-11-13 19:38:36 -0300

    Agrego el ejercicio 17 OO1

---
+++ ejercicio17-AlquilerDePropiedades/src/main/java/io/github/unlp_oo/ejercicio17_AlquilerDePropiedades/Propiedad.java
@@ -0,0 +1,64 @@
+package io.github.unlp_oo.ejercicio17_AlquilerDePropiedades;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import io.github.unlp_oo.ejercicio14_IntervaloDeTiempo.DateLapse;
+
+public class Propiedad {
+	
+	private String direccion;
+	private String nombre;
+	private double precioPorNoche;
+	private List<Reserva> reservas;
+	
+	public Propiedad(String direccion, String nombre, double precioPorNoche) {
+		this.direccion = direccion;
+		this.nombre = nombre;
+		this.precioPorNoche = precioPorNoche;
+		this.reservas = new ArrayList<Reserva>();
+	}
+	
+	
+	public boolean hayDisponibilidad(LocalDate ingreso, LocalDate egreso){
+		/* 
+		 * La función verifica que haya disponibilidad en las fechas pasadas como parametro.
+		 */
+		return this.reservas.stream().
+				anyMatch(reservas -> reservas.overLaps(new DateLapse(ingreso,egreso)));
+	}
+	
+	
+	public boolean crearReserva(LocalDate ingreso, LocalDate egreso) {
+		/*
+		 * Esta función lo que hace es crear una reserva , validando que no exista  
+		 * para agregarla a la lista.
+		 */
+		if ( this.hayDisponibilidad(ingreso, egreso) ) {
+			reservas.add(new Reserva(ingreso,egreso,this.precioPorNoche));
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean cancelar(Reserva reserva) {
+		/*
+		 * Recibimos una reserva y la borramos. (No hace falta recorrer para ver si existe)- Consultado.
+		*/	
+		return this.reservas.remove(reserva);
+	}
+	
+
+	public double gananciaEnPeriodo(LocalDate inicio, LocalDate fin) {
+		/* La función recibe dos periodos de tiempo y saca las ganancias del 
+		 * propietario en ese periodo. Recorriendo las reservas filtrando las 
+		 * que pertenecen a ese periodo para luego cada una de esas reservas saca su 
+		 * costo , al final sumo todo.
+		 */
+		
+		return  this.reservas.stream()
+				.filter(reservas -> reservas.overLaps(new DateLapse(inicio,fin)))
+				.mapToDouble(reservas -> reservas.precioDeReserva()).sum();
+	}
+}
---
+++ ejercicio17-AlquilerDePropiedades/src/main/java/io/github/unlp_oo/ejercicio17_AlquilerDePropiedades/Reserva.java
@@ -0,0 +1,31 @@
+package io.github.unlp_oo.ejercicio17_AlquilerDePropiedades;
+
+import java.time.LocalDate;
+
+import io.github.unlp_oo.ejercicio14_IntervaloDeTiempo.Date;
+import io.github.unlp_oo.ejercicio14_IntervaloDeTiempo.DateLapse;
+
+public class Reserva extends DateLapse {
+	
+	private double precioPorNoche;
+
+	public Reserva(LocalDate primera, LocalDate segunda, double precioPorNoche) {
+		super(primera, segunda);
+		this.precioPorNoche = precioPorNoche; 
+	}
+	
+	
+	
+	public boolean overLaps(DateLapse fecha) {
+		/**
+		Retorna true si el período de tiempo del receptor se superpone con el recibido por parámetro
+		**/
+        return (this.includesDate(fecha.getFrom()) || this.includesDate(fecha.getTo()));
+	}
+	
+	public double precioDeReserva(){
+		return this.precioPorNoche*this.sizeInDays();
+	}
+
+	
+}
